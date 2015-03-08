880f64cc063409858c72c314f86fe69872b35de6
Daniel Gruber <dgruber@univa.com>
2015-03-08 09:50:30 +0100

    DG: Added proxy for DRMAA1 for testing. Does not support most concepts.

---
+++ cmd/d1proxy/Godeps/_workspace/src/github.com/dgruber/drmaa/gestatus/gestatus.go
@@ -0,0 +1,359 @@
+/*
+   Copyright 2013 Daniel Gruber, info@gridengine.eu
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+
+      * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following disclaimer
+   in the documentation and/or other materials provided with the
+   distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+   This module contains Grid Engine specific code for getting much 
+   more detailed job information than plain DRMAA1 is able to show.
+   This is done by parsing the qstat -xml output.
+*/
+
+package gestatus
+
+import (
+   "github.com/dgruber/drmaa"
+   "github.com/dgruber/drmaa/gestatus/private_gestatus"
+   "strconv"
+   "strings"
+   "time"
+)
+
+type ClusterJobs struct {
+   jobs []private_gestatus.QstatJob
+}
+
+type Job struct {
+   Number    int64
+   Priority  float64
+   Name      string
+   Owner     string
+   State     string
+   StartTime string
+   QueueName string
+   JobClass  string
+   Slots     int64
+}
+
+func convertQstatJobToJob(qsj private_gestatus.QstatJob) (job Job) {
+   job.Number = qsj.JB_job_number
+   job.Priority = qsj.JAT_prio
+   job.Name = qsj.JB_name
+   job.Owner = qsj.JB_owner
+   job.State = qsj.State
+   qsj.JAT_start_time = strings.Replace(qsj.JAT_start_time, "T", "-", 1)
+   job.StartTime = qsj.JAT_start_time
+   job.QueueName = qsj.Queue_Name
+   job.JobClass = qsj.JClass_Name
+   job.Slots = qsj.Slots
+   return job
+}
+
+func GetClusterJobs() (clusterjobs ClusterJobs, err *drmaa.Error) {
+   if cjs, err := private_gestatus.GetClusterJobsStatus(); err == nil {
+      if cjs.JobList != nil {
+         clusterjobs.jobs = cjs.JobList
+         return clusterjobs, nil
+      } else {
+         return clusterjobs, nil
+      }
+   }
+   return clusterjobs, err
+}
+
+func (cjs *ClusterJobs) AllJobs() []Job {
+   if cjs.jobs == nil {
+      return nil
+   }
+
+   jobs := make([]Job, 0)
+
+   for _, j := range cjs.jobs {
+      jobs = append(jobs, convertQstatJobToJob(j))
+   }
+
+   return jobs
+}
+
+// ----------------------------------------------------------------------------
+
+type JobStatus struct {
+   /* hide internal job status */
+   js private_gestatus.InternalJobStatus
+}
+
+// Performs qstat -xml -j <jobid> and returns a JobStatus
+// object for this job.
+func GetJob(jobid string) (jobstat JobStatus, err error) {
+   js, err := private_gestatus.GetJobStatusById(jobid)
+   if err != nil {
+      return jobstat, err
+   }
+   jobstat.js = js
+
+   return jobstat, nil
+}
+
+// Returns the job status object, which contains all information
+// about a job. In case of any error it is nil and a drmaa error
+// is returned.
+func GetJobStatus(session *drmaa.Session, jobIds string) (jobstat JobStatus, err error) {
+   js, err := private_gestatus.GetJobStatus(session, jobIds)
+   if err != nil {
+      return jobstat, err
+   }
+   jobstat.js = js
+
+   return jobstat, nil
+}
+
+/* Exported access methods for the JobInfo struct */
+
+/* Returns the job name (given by -N submission option). */
+func (js *JobStatus) JobName() string {
+   return private_gestatus.GetJobName(&js.js)
+}
+
+/* Returns the Grid Engine job id number. */
+func (js *JobStatus) JobId() int64 {
+   return private_gestatus.GetJobNumber(&js.js)
+}
+
+/* this is the GE internatl script name which is executed on execd side */
+func (js *JobStatus) execFileName() string {
+   return private_gestatus.GetExecFileName(&js.js)
+}
+
+/* Returns the job script name as string. */
+func (js *JobStatus) JobScript() string {
+   return private_gestatus.GetScriptFile(&js.js)
+}
+
+/* Returns the job arguments as string slice. */
+func (js *JobStatus) JobArgs() []string {
+   return private_gestatus.GetJobArgs(&js.js)
+}
+
+/* Get owner of the job as string. */
+func (js *JobStatus) JobOwner() string {
+   return private_gestatus.GetOwner(&js.js)
+}
+
+/* Get ower of the job as Unix UID. */
+func (js *JobStatus) JobUID() int {
+   return private_gestatus.GetUID(&js.js)
+}
+
+func (js *JobStatus) JobGroup() string {
+   return private_gestatus.GetGroup(&js.js)
+}
+
+func (js *JobStatus) JobGID() int {
+   return private_gestatus.GetGID(&js.js)
+}
+
+func (js *JobStatus) JobAccountName() string {
+   return private_gestatus.GetAccount(&js.js)
+}
+
+func (js *JobStatus) IsImmediateJob() bool {
+   return private_gestatus.IsImmediate(&js.js)
+}
+
+func (js *JobStatus) HasReservation() bool {
+   return private_gestatus.IsReservation(&js.js)
+}
+
+// Returns the group of the job owner.
+func (js *JobStatus) IsBinaryJob() bool {
+   return private_gestatus.IsBinary(&js.js)
+}
+
+// Returns true if the job had requested -shell no.
+func (js *JobStatus) HasNoShell() bool {
+   return private_gestatus.IsNoShell(&js.js)
+}
+
+// Returns true in case the job is an array job.
+func (js *JobStatus) IsArrayJob() bool {
+   return private_gestatus.IsArray(&js.js)
+}
+
+/* Returns true if job merges stderr to stdout. */
+func (js *JobStatus) JobMergesStderr() bool {
+   return private_gestatus.IsMergeStderr(&js.js)
+}
+
+/* Returns true in case the job has memory binding requested. */
+func (js *JobStatus) HasMemoryBinding() bool {
+   if private_gestatus.GetMbind(&js.js) == "no_bind" {
+      return false
+   }
+   return true
+}
+
+/* Memory binding status. */
+func (js *JobStatus) MemoryBinding() string {
+   return private_gestatus.GetMbind(&js.js)
+}
+
+/* Start time of the job. */
+func (js *JobStatus) StartTime() time.Time {
+   return private_gestatus.GetStartTime(&js.js)
+}
+
+/* Since how long is the job running. */
+func (js *JobStatus) RunTime() time.Duration {
+   if js.StartTime().Unix() != 0 {
+      return time.Since(js.StartTime())
+   }
+   d, _ := time.ParseDuration("0s")
+   return d
+}
+
+/* Start time of a specific task of the job (for array jobs). */
+func (js *JobStatus) TaskStartTime(taskId int) time.Time {
+   return private_gestatus.GetTaskStartTime(&js.js, taskId)
+}
+
+/* End time of the job. ? */
+func (js *JobStatus) executionTime() time.Time {
+   return private_gestatus.GetExecutionTime(&js.js)
+}
+
+/* Submission time of the job. */
+func (js *JobStatus) SubmissionTime() time.Time {
+   return private_gestatus.GetSubmissionTime(&js.js)
+}
+
+/* The deadline of the job if set. */
+func (js *JobStatus) JobDeadline() time.Time {
+   return private_gestatus.GetDeadline(&js.js)
+}
+
+// The POSIX priority the job has requested.
+func (js *JobStatus) PosixPriority() int {
+   // priority is returned as positiv integer 1024 for 0
+   return private_gestatus.GetPosixPriority(&js.js) - 1024
+}
+
+// The mail options which determines on which event emails
+// about job status change is sent.
+func (js *JobStatus) MailOptions() string {
+   return private_gestatus.GetMailOptions(&js.js)
+}
+
+// The id of the advance reservation the job is running in.
+func (js *JobStatus) AdvanceReservationID() int {
+   return private_gestatus.GetAR(&js.js)
+}
+
+// The name of the requested job class.
+func (js *JobStatus) JobClassName() string {
+   return private_gestatus.GetJobClassName(&js.js)
+}
+
+// Returns all mail addresses information about the
+// job is sent to, depending on the mailing options.
+func (js *JobStatus) MailAdresses() []string {
+   return private_gestatus.GetMailingAdresses(&js.js)
+}
+
+// Returns hard resource requests as name and value
+func (js *JobStatus) HardRequests() ([]string, []string) {
+   return private_gestatus.GetHardRequests(&js.js)
+}
+
+func (js *JobStatus) gdilQueueNames(what string, task int) []string {
+   qil := make([]string, 16)
+
+   gdil := private_gestatus.GetGDIL(&js.js, task)
+
+   if gdil == nil {
+      return nil
+   }
+   for i := range *gdil {
+      if what == "QueueName" {
+         qil = append(qil, (*gdil)[i].QueueName)
+      } else if what == "HostName" {
+         qi := strings.Split((*gdil)[i].QueueName, "@")
+         if len(qi) == 2 {
+            qil = append(qil, qi[1])
+         }
+      } else if what == "Slots" {
+         qil = append(qil, strconv.Itoa((*gdil)[i].Slots))
+      }
+   }
+
+   return qil
+}
+
+// Returns all queue instance names where the job is running.
+// A queue instance contains a host and a queue part, where the
+// job is scheduled to.
+func (js *JobStatus) DestinationQueueInstanceList() []string {
+   return js.gdilQueueNames("QueueName", 0)
+}
+
+func (js *JobStatus) DestinationQueueInstanceListOfTask(task int) []string {
+   return js.gdilQueueNames("QueueName", task)
+}
+
+func (js *JobStatus) DestinationSlotsList() []string {
+   return js.gdilQueueNames("Slots", 0)
+}
+
+// Returns all host names where the job (the first task in case of array jobs)
+// is running.
+func (js *JobStatus) DestinationHostList() []string {
+   return js.gdilQueueNames("HostName", 0)
+}
+
+func (js *JobStatus) DestinationHostListOfTask(task int) []string {
+   return js.gdilQueueNames("HostName", task)
+}
+
+func (js *JobStatus) TasksCount() int {
+   return private_gestatus.GetTaskCount(&js.js)
+}
+
+func (js *JobStatus) ParallelEnvironment() string {
+   return private_gestatus.GetParallelEnvironmentRequest(&js.js)
+}
+
+func (js *JobStatus) ParallelEnvironmentMin() int64 {
+   return private_gestatus.GetParallelEnvironmentMin(&js.js)
+}
+
+func (js *JobStatus) ParallelEnvironmentMax() int64 {
+   return private_gestatus.GetParallelEnvironmentMax(&js.js)
+}
+
+func (js *JobStatus) ParallelEnvironmentStep() int64 {
+   return private_gestatus.GetParallelEnvironmentStep(&js.js)
+}
+
+func (js *JobStatus) ResourceUsage(task int) ([]string, []string) {
+   return private_gestatus.GetUsageList(&js.js, task)
+}
---
+++ cmd/d1proxy/d1proxy.go
@@ -0,0 +1,295 @@
+/*
+   Copyright 2015 Daniel Gruber, Univa, Blog: www.gridengine.eu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+// This is a simple proxy for ubercluster which supports all DRAMA1
+// compatible DRM systems (Grid Engine, etc.).
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dgruber/drmaa"
+	"github.com/dgruber/ubercluster"
+	"gopkg.in/alecthomas/kingpin.v1"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var verbose bool = false
+
+func init() {
+	if verbose == false {
+		log.SetOutput(ioutil.Discard)
+	}
+}
+
+// Standard set of CLI parameters.
+var (
+	app        = kingpin.New("d1proxy", "A proxy server for DRMAA1 compatible cluster schedulers (like Univa Grid Engine).")
+	cliVerbose = app.Flag("verbose", "Enables enhanced logging for debugging.").Bool()
+	cliPort    = app.Flag("port", "Sets address and port on which proxy is listening.").Default(":8888").String()
+	certFile   = app.Flag("certFile", "Path to certification file for secure connections (TLS).").Default("").String()
+	keyFile    = app.Flag("keyFile", "Path to key file for secure connections (TLS).").Default("").String()
+	otp        = app.Flag("otp", "One time password settings (\"yubikey\") or a fixed shared secret.").Default("").String()
+)
+
+// drmaa1Proxy is our internal DRMAA1 DRMS implementation.
+type drmaa1Proxy struct {
+	Session drmaa.Session
+}
+
+// convertDRMAAJobTemplate transforms a DRMAA2 job template (from ubercluster package)
+// into a DRMAA1 job template which is used for executing the job
+func convertDRMAAJobTemplate(s drmaa.Session, jt ubercluster.JobTemplate) (*drmaa.JobTemplate, error) {
+	djt, err := s.AllocateJobTemplate()
+	if err != nil {
+		log.Println("Error during job template allocation: ", err)
+		return nil, err
+	}
+	if err := djt.SetJobName(jt.JobName); err != nil {
+		log.Println("Error during SetJobName: ", err)
+	}
+	if err := djt.SetRemoteCommand(jt.RemoteCommand); err != nil {
+		log.Println("Error during SetRemoteCommand: ", err)
+	}
+	if err := djt.SetArgs(jt.Args); err != nil {
+		log.Println("Error during SetArgs: ", err)
+	}
+	// TODO we have more parameters
+	return &djt, nil
+}
+
+// RunJob runs a DRMAA job in the cluster. Is required in order to fulfill
+// the  ProxyImplementer interface.
+func (dp *drmaa1Proxy) RunJob(template ubercluster.JobTemplate) (string, error) {
+	// file path fix when the app is uploaded
+	localFile := template.WorkingDirectory + "/" + template.RemoteCommand
+	log.Println("Local file: ", localFile)
+	if fi, err := os.Stat(localFile); err == nil {
+		if fi.IsDir() == false {
+			// since we have a file in staging area we execute it :/
+			log.Println("Adding path to remote command")
+			template.RemoteCommand = localFile
+		}
+	}
+	if jt, err := convertDRMAAJobTemplate(dp.Session, template); err != nil {
+		log.Println("Error during job template conversion: ", err)
+		return "", err
+	} else {
+		if jobid, err := dp.Session.RunJob(jt); err != nil {
+			return "", err
+		} else {
+			return jobid, nil
+		}
+	}
+	// unreachable
+	return "NOOP", nil
+}
+
+// JobOperation changes the state of a job in the system. Is required by the
+// ProxyImplementer interface.
+func (dp *drmaa1Proxy) JobOperation(jobsessionname, operation, jobid string) (string, error) {
+	// in DRMAA1 we irgnore the job session name since we don't have any
+	switch operation {
+	case "suspend":
+		if err := dp.Session.SuspendJob(jobid); err != nil {
+			return "", err
+		}
+		return "Suspended job", nil
+	case "resume":
+		if err := dp.Session.ResumeJob(jobid); err != nil {
+			return "", err
+		}
+		return "Resumed job", nil
+	case "terminate":
+		if err := dp.Session.TerminateJob(jobid); err != nil {
+			return "", err
+		}
+		return "Terminated job", nil
+		// TODO adding hold and resume
+	default:
+		log.Println("JobOperation unknown operation ", operation)
+		return "", errors.New("Unknown operation: " + operation)
+	}
+	// unreachable - go ...
+	return "NOOP", nil
+}
+
+// GetJobInfosByFilter is not available in DRMAA1. Is required by the
+// ProxyImplementer interface.
+func (dp *drmaa1Proxy) GetJobInfosByFilter(filtered bool, filter ubercluster.JobInfo) []ubercluster.JobInfo {
+	// filtering is not supported in DRMAA1
+	return nil
+}
+
+// convertDRMAAState converts a DRMAA state into a DRMAA2 state
+func convertDRMAAStateString(ds string) ubercluster.JobState {
+	switch ds {
+	case "Undetermined":
+		return ubercluster.Undetermined
+	case "QueuedActive":
+		return ubercluster.Queued
+	case "SystemHold":
+		return ubercluster.QueuedHeld
+	case "UserHold":
+		return ubercluster.QueuedHeld
+	case "UserSystemHold":
+		return ubercluster.QueuedHeld
+	case "Running":
+		return ubercluster.Running
+	case "SystemSuspended":
+		return ubercluster.Suspended
+	case "UserSuspended":
+		return ubercluster.Suspended
+	case "UserSystemSuspended":
+		return ubercluster.Suspended
+	case "Done":
+		return ubercluster.Done
+	case "Failed":
+		return ubercluster.Failed
+	}
+	return ubercluster.Undetermined
+}
+
+func convertDRMAAState(ds drmaa.PsType) ubercluster.JobState {
+	switch ds {
+	case drmaa.PsUndetermined:
+		return ubercluster.Undetermined
+	case drmaa.PsQueuedActive:
+		return ubercluster.Queued
+	case drmaa.PsSystemOnHold:
+		return ubercluster.QueuedHeld
+	case drmaa.PsUserOnHold:
+		return ubercluster.QueuedHeld
+	case drmaa.PsUserSystemOnHold:
+		return ubercluster.QueuedHeld
+	case drmaa.PsRunning:
+		return ubercluster.Running
+	case drmaa.PsSystemSuspended:
+		return ubercluster.Suspended
+	case drmaa.PsUserSuspended:
+		return ubercluster.Suspended
+	case drmaa.PsUserSystemSuspended:
+		return ubercluster.Suspended
+	case drmaa.PsDone:
+		return ubercluster.Done
+	case drmaa.PsFailed:
+		return ubercluster.Failed
+	}
+	return ubercluster.Undetermined
+}
+
+// GetJobInfo returns information about a job. Probablamtic in
+// DRMAA1 since you have to wait for the job being finished.
+func (dp *drmaa1Proxy) GetJobInfo(jobid string) *ubercluster.JobInfo {
+	// assume that jobid is PID
+	if state, err := dp.Session.JobPs(jobid); err != nil {
+		log.Println(err)
+		return nil
+	} else {
+		// no more information availble in DRMAA1!
+		var ji ubercluster.JobInfo
+		ji.State = convertDRMAAState(state)
+		ji.Id = jobid
+		// TODO plugin for vendors to make it more useful?
+		return &ji
+	}
+	// unreachable - go ...
+	return nil
+}
+
+// GetAllMachines is not available in DRMAA.
+func (dp *drmaa1Proxy) GetAllMachines(machines []string) ([]ubercluster.Machine, error) {
+	// no machines in DRMAA1 -> we need to call the DRM system
+	return nil, nil
+}
+
+// GetAllQueues is not really helpful since there is no notion of queues
+// in DRMAA1.
+func (dp *drmaa1Proxy) GetAllQueues(queues []string) ([]ubercluster.Queue, error) {
+	// no queues in DRMAA1
+	var q ubercluster.Queue
+	q.Name = "DRMAA"
+	return []ubercluster.Queue{q}, nil
+}
+
+// GetAllSessions just returns one session name since there is just one
+// (unnamed) session possible in DRMAA1.
+func (dp *drmaa1Proxy) GetAllSessions(session []string) ([]string, error) {
+	// only one session in DRMAA1
+	allsessions := make([]string, 0, 0)
+	allsessions = append(allsessions, "DRMAA")
+	return allsessions, nil
+}
+
+// GetAllCategories returns nothing since there is no category listing
+// available in DRMAA1.
+func (dp *drmaa1Proxy) GetAllCategories() ([]string, error) {
+	// no real catgegories in DRMAA1
+	return nil, nil
+}
+
+// DRMSVersion returns the version of the DRMAA implementation.
+func (dp *drmaa1Proxy) DRMSVersion() string {
+	return dp.Session.GetDrmaaImplementation()
+}
+
+// DRMSName returns the name of the DRM (like Univa Grid Engine or Sun Grid Engine)
+func (dp *drmaa1Proxy) DRMSName() string {
+	sys, _ := dp.Session.GetDrmSystem()
+	return sys
+}
+
+func (lp *drmaa1Proxy) DRMSLoad() float64 {
+	// TODO some ratio about pending / running jobs respecting the
+	// throughput
+	return 0.5
+}
+
+// InitDRMAA opens a DRMAA session which is going to be used
+// by the callbacks.
+func InitDRMAA() (drmaa1Proxy, error) {
+	var d1p drmaa1Proxy
+	s, err := drmaa.MakeSession()
+	if err != nil {
+		log.Panic(err)
+		os.Exit(1)
+	} else {
+		// add session
+		d1p.Session = s
+	}
+	return d1p, nil
+}
+
+func main() {
+	var err error
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	if *cliVerbose {
+		log.SetOutput(os.Stdout)
+	}
+
+	d1, err := InitDRMAA()
+	if err != nil {
+		fmt.Println("Error during initialization of DRMAA: ", err)
+		os.Exit(1)
+	}
+
+	ubercluster.ProxyListenAndServe(*cliPort, *certFile, *keyFile, *otp, &d1)
+	defer d1.Session.Exit()
+}
